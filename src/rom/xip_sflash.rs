c782e14f08b4dcafd79ea753314c208ef68b4c37
9names <60134748+9names@users.noreply.github.com>
2021-04-03 01:13:47 +1100

    Copied xip_sflash func comments from SDK

--- src/rom/xip_sflash.rs
+++ src/rom/xip_sflash.rs
@@ -2,6 +2,16 @@ use crate::rom_lookup::{rom_lookup, RomIndex};
 use crate::rom::BL_Err_Type;
 use crate::rom::SPI_Flash_Cfg_Type;
 
+/****************************************************************************//**
+ * Save flash controller state
+ *
+ * `pFlashCfg` - Flash config pointer
+ *
+ * `offset` - CPU XIP flash offset pointer
+ *
+ * Returns SUCCESS or ERROR
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_State_Save(pFlashCfg: *mut SPI_Flash_Cfg_Type, offset: *mut u32) -> BL_Err_Type {
     let romdriver_func = unsafe {
@@ -13,6 +23,16 @@ pub fn XIP_SFlash_State_Save(pFlashCfg: *mut SPI_Flash_Cfg_Type, offset: *mut u3
     romdriver_func(pFlashCfg, offset)
 }
 
+/****************************************************************************//**
+ * Restore flash controller state
+ *
+ * `pFlashCfg` - Flash config pointer
+ *
+ * `offset` - CPU XIP flash offset
+ *
+ * Returns SUCCESS or ERROR
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_State_Restore(pFlashCfg: *mut SPI_Flash_Cfg_Type, offset: u32) -> BL_Err_Type {
     let romdriver_func = unsafe {
@@ -23,6 +43,18 @@ pub fn XIP_SFlash_State_Restore(pFlashCfg: *mut SPI_Flash_Cfg_Type, offset: u32)
     romdriver_func(pFlashCfg, offset)
 }
 
+/****************************************************************************//**
+ * Erase flash one region
+ *
+ * `pFlashCfg` - Flash config pointer
+ *
+ * `startaddr` - start address to erase
+ *
+ * `endaddr` - end address(include this address) to erase
+ *
+ * Returns SUCCESS or ERROR
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_Erase_Need_Lock(
     pFlashCfg: *mut SPI_Flash_Cfg_Type,
@@ -38,6 +70,20 @@ pub fn XIP_SFlash_Erase_Need_Lock(
     romdriver_func(pFlashCfg, startaddr, endaddr)
 }
 
+/****************************************************************************//**
+ * Program flash one region
+ *
+ * `pFlashCfg` - Flash config pointer
+ *
+ * `addr` - start address to be programed
+ *
+ * `data` - data pointer to be programed
+ *
+ * `len` - data length to be programed
+ *
+ * Returns SUCCESS or ERROR
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_Write_Need_Lock(
     pFlashCfg: *mut SPI_Flash_Cfg_Type,
@@ -54,6 +100,20 @@ pub fn XIP_SFlash_Write_Need_Lock(
     romdriver_func(pFlashCfg, addr, data, len)
 }
 
+/****************************************************************************//**
+ * Read data from flash
+ *
+ * `pFlashCfg` - Flash config pointer
+ *
+ * `addr` - flash read start address
+ *
+ * `data` - data pointer to store data read from flash
+ *
+ * `len` - data length to read
+ *
+ * Returns SUCCESS or ERROR
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_Read_Need_Lock(
     pFlashCfg: *mut SPI_Flash_Cfg_Type,
@@ -70,6 +130,16 @@ pub fn XIP_SFlash_Read_Need_Lock(
     romdriver_func(pFlashCfg, addr, data, len)
 }
 
+/****************************************************************************//**
+ * Get Flash Jedec ID
+ *
+ * `pFlashCfg` - Flash config pointer
+ *
+ * `data` - data pointer to store Jedec ID Read from flash
+ *
+ * Returns SUCCESS or ERROR
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_GetJedecId_Need_Lock(
     pFlashCfg: *mut SPI_Flash_Cfg_Type,
@@ -84,6 +154,16 @@ pub fn XIP_SFlash_GetJedecId_Need_Lock(
     romdriver_func(pFlashCfg, data)
 }
 
+/****************************************************************************//**
+ * Get Flash Device ID
+ *
+ * `pFlashCfg` - Flash config pointer
+ *
+ * `data` - data pointer to store Device ID Read from flash
+ *
+ * Returns SUCCESS or ERROR
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_GetDeviceId_Need_Lock(
     pFlashCfg: *mut SPI_Flash_Cfg_Type,
@@ -98,6 +178,18 @@ pub fn XIP_SFlash_GetDeviceId_Need_Lock(
     romdriver_func(pFlashCfg, data)
 }
 
+/****************************************************************************//**
+ * Get Flash Unique ID
+ *
+ * `pFlashCfg` - Flash config pointer
+ *
+ * `data` - data pointer to store Device ID Read from flash
+ *
+ * `idLen` - Unique id len
+ *
+ * Returns SUCCESS or ERROR
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_GetUniqueId_Need_Lock(
     pFlashCfg: *mut SPI_Flash_Cfg_Type,
@@ -113,6 +205,18 @@ pub fn XIP_SFlash_GetUniqueId_Need_Lock(
     romdriver_func(pFlashCfg, data, idLen)
 }
 
+/****************************************************************************//**
+ * Read data from flash via XIP
+ *
+ * `addr` - flash read start address
+ *
+ * `data` - data pointer to store data read from flash
+ *
+ * `len` - data length to read
+ *
+ * Returns SUCCESS or ERROR
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_Read_Via_Cache_Need_Lock(addr: u32, data: *mut u8, len: u32) -> BL_Err_Type {
     let romdriver_func = unsafe {
@@ -123,6 +227,21 @@ pub fn XIP_SFlash_Read_Via_Cache_Need_Lock(addr: u32, data: *mut u8, len: u32) -
     romdriver_func(addr, data, len)
 }
 
+
+/****************************************************************************//**
+ * Read data from flash with lock
+ *
+ * `pFlashCfg` - Flash config pointer
+ *
+ * `addr` - flash read start address
+ *
+ * `dst` - data pointer to store data read from flash
+ *
+ * `len` - data length to read
+ *
+ * Returns 0
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_Read_With_Lock(
     pFlashCfg: *mut SPI_Flash_Cfg_Type,
@@ -139,6 +258,20 @@ pub fn XIP_SFlash_Read_With_Lock(
     romdriver_func(pFlashCfg, addr, dst, len)
 }
 
+/****************************************************************************//**
+ * Program flash one region with lock
+ *
+ * `pFlashCfg` - Flash config pointer
+ *
+ * `addr` - Start address to be programed
+ *
+ * `src` - Data pointer to be programed
+ *
+ * `len` - Data length to be programed
+ *
+ * Returns 0
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_Write_With_Lock(
     pFlashCfg: *mut SPI_Flash_Cfg_Type,
@@ -155,6 +288,18 @@ pub fn XIP_SFlash_Write_With_Lock(
     romdriver_func(pFlashCfg, addr, src, len)
 }
 
+/****************************************************************************//**
+ * Erase flash one region with lock
+ *
+ * `pFlashCfg` - Flash config pointer
+ *
+ * `addr` - Start address to be erased
+ *
+ * `len` - Data length to be erased
+ *
+ * Returns 0
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_Erase_With_Lock(pFlashCfg: *mut SPI_Flash_Cfg_Type, addr: u32, len: u32) -> u32 {
     let romdriver_func = unsafe {
@@ -166,6 +311,12 @@ pub fn XIP_SFlash_Erase_With_Lock(pFlashCfg: *mut SPI_Flash_Cfg_Type, addr: u32,
     romdriver_func(pFlashCfg, addr, len)
 }
 
+/****************************************************************************//**
+ * XIP SFlash option save
+ *
+ * `aesEnable` - AES enable status pointer
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_Opt_Enter(aesEnable: *mut u8) {
     let romdriver_func = unsafe {
@@ -176,6 +327,12 @@ pub fn XIP_SFlash_Opt_Enter(aesEnable: *mut u8) {
     romdriver_func(aesEnable)
 }
 
+/****************************************************************************//**
+ * XIP SFlash option restore
+ *
+ * `aesEnable` - AES enable status
+ *
+*******************************************************************************/
 #[inline(always)]
 pub fn XIP_SFlash_Opt_Exit(aesEnable: u8) {
     let romdriver_func = unsafe {
