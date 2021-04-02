4a3799c987c72ac12eecb383993c0ffc492bd522
9names <60134748+9names@users.noreply.github.com>
2021-04-02 21:03:44 +1100

    Add the rest of the sf_cfg functions

--- src/sflash/sf_cfg.rs
+++ src/sflash/sf_cfg.rs
@@ -1,9 +1,27 @@
 // mod rom_lookup;
 use crate::rom_lookup::{rom_lookup, RomIndex};
-use crate::sflash::SF_Ctrl_Mode_Type;
+use crate::sflash::{BL_Err_Type, SF_Ctrl_Mode_Type, SF_Ctrl_Owner_Type, SPI_Flash_Cfg_Type};
 
-use crate::sflash::BL_Err_Type;
-use crate::sflash::SF_Ctrl_Owner_Type;
+pub fn SF_Cfg_Get_Flash_Cfg_Need_Lock(
+    flashID: u32,
+    pFlashCfg: *mut SPI_Flash_Cfg_Type,
+) -> BL_Err_Type {
+    let romdriver_func = unsafe {
+        core::mem::transmute::<*const (), extern "C" fn(u32, *mut SPI_Flash_Cfg_Type) -> u32>(
+            rom_lookup(RomIndex::SF_Cfg_Get_Flash_Cfg_Need_Lock),
+        )
+    };
+    romdriver_func(flashID, pFlashCfg)
+}
+
+pub fn SF_Cfg_Restore_GPIO17_Fun(fun: u8) {
+    let romdriver_func = unsafe {
+        core::mem::transmute::<*const (), extern "C" fn(u8)>(rom_lookup(
+            RomIndex::SF_Cfg_Restore_GPIO17_Fun,
+        ))
+    };
+    romdriver_func(fun)
+}
 
 #[inline(always)]
 pub fn SF_Cfg_Flash_Identify(
@@ -28,6 +46,15 @@ pub fn SF_Cfg_Flash_Identify(
     )
 }
 
+pub fn SF_Cfg_Init_Ext_Flash_Gpio(extFlashPin: u8) {
+    let romdriver_func = unsafe {
+        core::mem::transmute::<*const (), extern "C" fn(u8)>(rom_lookup(
+            RomIndex::SF_Cfg_Init_Ext_Flash_Gpio,
+        ))
+    };
+    romdriver_func(extFlashPin)
+}
+
 #[inline(always)]
 pub fn SF_Cfg_Init_Flash_Gpio(flashPinCfg: u8, restoreDefault: u8) {
     let romdriver_func = unsafe {
@@ -36,6 +63,23 @@ pub fn SF_Cfg_Init_Flash_Gpio(flashPinCfg: u8, restoreDefault: u8) {
         ))
     };
     romdriver_func(flashPinCfg, restoreDefault)
+}
+
+pub fn SF_Cfg_Init_Internal_Flash_Gpio() {
+    let romdriver_func = unsafe {
+        core::mem::transmute::<*const (), extern "C" fn()>(rom_lookup(
+            RomIndex::SF_Cfg_Init_Internal_Flash_Gpio,
+        ))
+    };
+    romdriver_func()
+}
+pub fn SF_Cfg_Deinit_Ext_Flash_Gpio(extFlashPin: u8) {
+    let romdriver_func = unsafe {
+        core::mem::transmute::<*const (), extern "C" fn(u8)>(rom_lookup(
+            RomIndex::SF_Cfg_Deinit_Ext_Flash_Gpio,
+        ))
+    };
+    romdriver_func(extFlashPin)
 }
 
 #[inline(always)]
