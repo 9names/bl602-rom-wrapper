8e51b9e35793fa6635b7cbf6da1ea53b7262d3fb
9names <60134748+9names@users.noreply.github.com>
2021-04-04 14:06:12 +1000

    Add SF_Ctrl_Get_Flash_Image_Offset

--- src/rom/sf_ctrl.rs
+++ src/rom/sf_ctrl.rs
@@ -12,6 +12,17 @@ pub fn SF_Ctrl_Set_Owner(owner: SF_Ctrl_Owner_Type) {
     romdriver_func(owner)
 }
 
+// NOTE: the SF_Ctrl_Set_Flash_Image_Offset and SF_Ctrl_Get_Flash_Image_Offset functions would
+// be good candidates for replacing with Rust calls the bl602-hal as they're just register lookups
+// The corresponding register is SF_CTRL_BASE.SF_CTRL_SF_ID0_OFFSET, and it takes a u32
+
+/// Set the starting location in SPI Flash for mapping into CPU address space
+///
+/// `offset` - the offset from the start of SPI flash to where the first memory
+/// mapped region starts.
+///
+/// *Example*: SF_Ctrl_Set_Flash_Image_Offset(0) will make SPI address 0x0
+/// correspond to 0x2300_0000 in CPU address space
 #[inline(always)]
 pub fn SF_Ctrl_Set_Flash_Image_Offset(offset: u32) {
     let romdriver_func = unsafe {
@@ -20,4 +31,18 @@ pub fn SF_Ctrl_Set_Flash_Image_Offset(offset: u32) {
         ))
     };
     romdriver_func(offset)
+}
+
+// uint32_t ATTR_TCM_SECTION SF_Ctrl_Get_Flash_Image_Offset(void)
+// {
+//     return BL_RD_REG(SF_CTRL_BASE,SF_CTRL_SF_ID0_OFFSET);
+// }
+#[inline(always)]
+pub fn SF_Ctrl_Get_Flash_Image_Offset() -> u32 {
+    let romdriver_func = unsafe {
+        core::mem::transmute::<*const (), extern "C" fn()->u32 >(rom_lookup(
+            RomIndex::SF_Ctrl_Get_Flash_Image_Offset,
+        ))
+    };
+    romdriver_func()
 }
